8257c9888ac2697b8d791872f6fdcb2d14d0822f
rattle99 <rattle.travian@gmail.com>
2016-12-26 17:14:14 +0530

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+
+#include "morse.h"
+#include <stdio.h>
+#include <cstring>
+
+int main() {
+	setgpiopin(1); // Set the GPIO pin to GPIO_GEN1.
+	makecommands(); 
+	char str[25]; 
+	printf(" Enter message : ");
+	gets(str);	//Input string
+	blinkstring(str); //Function call for output in morsecode.
+	
+	return 0;
+}
