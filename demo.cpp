ad60677f7e966d485a9928e9106a31a4192b7cd5
rattle99 <rattle.travian@gmail.com>
2016-12-26 18:30:47 +0530

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -2,13 +2,14 @@
 #include "cmorse"
 #include <cstdio>
 #include <cstring>
+#include <iostream>
 
 int main() {
 	setgpiopin(1); // Set the GPIO pin to GPIO_GEN1.
 	makecommands(); 
-	char str[25]; 
+	char str[50]; 
 	printf(" Enter message : ");
-	gets(str);	//Input string
+	std::cin.getline(str,50);	//Input string
 	blinkstring(str); //Function call for output in morsecode.
 	
 	return 0;
