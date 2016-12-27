5ab60f7ea5ee85d748c086166a90d6847f5a80a1
rattle99 <rattle.travian@gmail.com>
2016-12-27 21:48:02 +0530

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -3,14 +3,29 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 
-int main() {
+using namespace std;
+
+int main() 
+{
 	setgpiopin(1); // Set the GPIO pin to GPIO_GEN1.
 	makecommands(); 
-	char str[50]; 
+	char str[100]; 
 	printf(" Enter message : ");
-	std::cin.getline(str,50);	//Input string
+	ofstream fout;
+	fout.open("Message-log.txt",ios::app);
+	cin.getline(str,99);	//Input string
+	fout<<str<<endl;
 	blinkstring(str); //Function call for output in morsecode.
-	
+	string data;
+	ifstream fin; 
+	fin.open("Message-log.txt");
+	while(!fin.eof())
+	{
+		fin>>data; 
+		cout<<data<<endl;
+	}
+	fin.close();
 	return 0;
 }
